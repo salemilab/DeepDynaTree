f1c2d3face61bbff84f74d92f68d9129acfcec0d
HamelnSun <50168019+HamelnSun@users.noreply.github.com>
2022-04-06 14:59:57 -0400

    saving inforation

---
+++ dl/info.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env
+# -*- coding: utf-8 -*-
+
+"""
+info.py: 
+"""
+
+SAVE_PATH = "./nov_save"
+
+METRICS = ["loss"]
+MIN_METRICS = ["loss"]
+MAX_METRICS = []
+SUMMARY_ITEMS = ["lr", "acc", "balance_acc", "f1_weighted", "f1_macro", "pr", "recall", "brier_score", "cross_entropy", "macro_auc_ovo", "weighted_auc_ovo",
+                 "macro_auc_ovr", "weighted_auc_ovr"]
